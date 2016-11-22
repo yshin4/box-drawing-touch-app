68095615ad38c58dc2c161e30d12d0e7512c21b0
Francis Gow <fgow@lion.lmu.edu>
2016-11-22 10:56:11 -0800

    just some spacing

--- boxes-with-physics.js
+++ boxes-with-physics.js
@@ -18,13 +18,11 @@
             setupDragState();
         });
     }
-
-
+    
     /**
      * Tracks a box as it is rubberbanded or moved across the drawing area.
      */
     let trackDrag = (event) => {
-
         $.each(event.changedTouches, function (index, touch) {
             // Don't bother if we aren't tracking anything.
             if(touch.target.drawingBox){
@@ -32,14 +30,11 @@
                     left: (touch.target.anchorX < touch.pageX) ? touch.target.anchorX : touch.pageX,
                     top: (touch.target.anchorY < touch.pageY) ? touch.target.anchorY : touch.pageY
                 };
-
             touch.target.drawingBox
                 .data({position: newOffset})
                 .offset(newOffset)
                 .width(Math.abs(touch.pageX - touch.target.anchorX))
                 .height(Math.abs(touch.pageY - touch.target.anchorY));
-
-
             } else if (touch.target.movingBox) {
                 // $("p.log").text("MOVING! " + Date.now() + " " + event.target);
                 // Reposition the object.
@@ -47,16 +42,13 @@
                     left: touch.pageX - touch.target.deltaX,
                     top: touch.pageY - touch.target.deltaY
                 };
-
                 if (newPosition.left <= 25 && newPosition.top <= 25) {
                     touch.target.remove();
                 }
-
                 $(touch.target).data('position', newPosition);
                 touch.target.movingBox.offset(newPosition);
             }
         });
-
         // Don't do any touch scrolling.
         event.preventDefault();
     };
@@ -67,7 +59,6 @@
     let endDrag = (event) => {
         $("p.log").text("");
         $.each(event.changedTouches, (index, touch) => {
-
             if (touch.target.drawingBox){
                 $($("div.box")).each((index, element) => {
                   element.addEventListener("touchstart", startMove, false);
@@ -77,15 +68,12 @@
                   element.addEventListener("gesturechange", changeResize, false);
                   element.addEventListener("gestureend", endResize, false);
                 });
-
                 touch.target.drawingBox = null;
             } else if (touch.target.movingBox) {
                 // Change state to "not-moving-anything" by clearing out
                 // touch.target.movingBox.
-
                 touch.target.movingBox = null;
             }
-
             $(".drawing-area .box")
                 .removeClass("box-highlight")
                 .each((index, element) => {
@@ -100,7 +88,6 @@
      */
     let unhighlight = (event) => $(event.currentTarget).removeClass("box-highlight");
     let highlight = (event) => $(event.currentTarget).addClass("box-highlight");
-
     /**
      * Begins a box move sequence.
      */
@@ -108,31 +95,25 @@
         $.each(event.changedTouches, (index, touch) => {
             // Highlight the element.
             $(touch.target).addClass("box-highlight");
-
             // Take note of the box's current (global) location. Also, set its velocity and acceleration to
             // nothing because, well, _finger_.
-
             let jThis = $(touch.target);
             let startOffset = jThis.offset();
-
             jThis.data({
                 position: startOffset,
                 velocity: { x: 0, y: 0, z: 0 },
                 acceleration: { x: 0, y: 0, z: 0 }
             });
-
             // Set the drawing area's state to indicate that it is
             // in the middle of a move.
             touch.target.movingBox = jThis;
             touch.target.deltaX = touch.pageX - startOffset.left;
             touch.target.deltaY = touch.pageY - startOffset.top;
         });
-
         // Eat up the event so that the drawing area does not
         // deal with it.
         event.stopPropagation();
     };
-
     let startResize = (event) =>{
         event.preventDefault();
         let currentBox = $(event.currentTarget);
@@ -140,29 +121,23 @@
             currentWidth: currentBox.width(),
             currentHeight: currentBox.height()
         });
-
        $(".drawing-area").each((index, element) => {
            element.removeEventListener("touchmove", trackDrag);
            element.removeEventListener("touchstart", startDraw);
-
        });
-
     };
-
     let changeResize = (event) => {
         event.preventDefault();
         let currentBox = $(event.currentTarget);
         currentBox.width(currentBox.data("currentWidth") * event.scale);
         currentBox.height(currentBox.data("currentHeight") * event.scale);
     };
-
     let endResize = (event) => {
         event.preventDefault();
        $(".drawing-area").each((index, element) => {
            element.addEventListener("touchmove", trackDrag, false);
        });
     };
-
     /**
      * The motion update routine.
      */
@@ -176,16 +151,13 @@
         if (!lastTimestamp) {
             lastTimestamp = timestamp;
         }
-
         // Keep that frame rate under control.
         if (timestamp - lastTimestamp < FRAME_DURATION) {
             window.requestAnimationFrame(updateBoxes);
             return;
         }
-
         $($("div.box")).each((index, element) => {
             let $element = $(element);
-
             // If it's highlighted, we don't accelerate it because it is under a finger.
             if ($element.hasClass("box-highlight")) {
                 return;
@@ -220,15 +192,12 @@
                 s.left = (s.left <= bounds.left) ? bounds.left : bounds.right - $element.width();
                 v.x = -v.x;
             }
-
             if ((s.top <= bounds.top) || (s.top + $element.height() > bounds.bottom)) {
                 s.top = (s.top <= bounds.top) ? bounds.top : bounds.bottom - $element.height();
                 v.y = -v.y;
             }
-
             $(element).offset(s);
         });
-
         lastTimestamp = timestamp;
         window.requestAnimationFrame(updateBoxes);
     };
@@ -247,14 +216,12 @@
                 element.addEventListener("touchend", endDrag, false);
                 element.addEventListener("touchstart", startDraw, false);
             })
-
             .find($("div.box")).each((index, element) => {
                 element.addEventListener("touchstart", startMove, false);
                 element.addEventListener("touchend", unhighlight, false);
                 element.addEventListener("gesturestart", startResize, false);
                 element.addEventListener("gesturechange", changeResize, false);
                 element.addEventListener("gestureend", endResize, false);
-
                 $(element).data({
                     position: $(element).offset(),
                     velocity: { x: 0, y: 0, z: 0 },
